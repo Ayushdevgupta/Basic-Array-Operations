99db54e1d82fb33439c97c206c171c5c0c8e0b90
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-01-11 17:48:41 +0530

    Show efficient approach

--- maxDifference.cpp
+++ maxDifference.cpp
@@ -1,16 +1,41 @@
 //Naive Approach
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
+// int maxDiff(int arr[], int n)
+// {
+//     int res = arr[1] - arr[0];
+//     for(int i = 0; i < n-1; i++)
+//     {
+//         for(int j = i + 1; j <n; j++)
+//         {
+//             res = max(res,arr[j] - arr[i]);
+//         }
+//     }
+//     return res;
+    
+// }
+
+
+
+// int main()
+// {
+//     int arr[] = {2,3,10,6,4,8,1};
+//     cout<<maxDiff(arr,7);
+// }
+
+//Efficient Approach
+#include<iostream>
+#include<algorithm>
+using namespace std;
 int maxDiff(int arr[], int n)
 {
     int res = arr[1] - arr[0];
-    for(int i = 0; i < n-1; i++)
+    int minVal = arr[0];
+    for(int i = 1; i < n; i++)
     {
-        for(int j = i + 1; j <n; j++)
-        {
-            res = max(res,arr[j] - arr[i]);
-        }
+        res = max(res,arr[i] - minVal);
+        minVal = min(minVal,arr[i]);
     }
     return res;
     
@@ -20,8 +45,6 @@ int maxDiff(int arr[], int n)
 
 int main()
 {
-    int arr[] = {2,3,10,6,4,8,1};
-    cout<<maxDiff(arr,7);
+    int arr[] = {10,20,30};
+    cout<<maxDiff(arr,3);
 }
-
-//Efficient Approach
