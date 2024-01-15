8da92a7bb9e5b440c51eb52d3fe5a8bed9e09aea
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-01-15 21:15:31 +0530

    print max consecutive ones

--- maxConsecutiveOnes.cpp
+++ maxConsecutiveOnes.cpp
@@ -1,26 +1,54 @@
+//Naive Solution
+// #include<iostream>
+// using namespace std;
+// int getMax(bool arr[], int n)
+// {
+//    int res = 0;
+//    for(int i = 0; i < n; i++)
+//    {
+//     int curr = 0;
+//     for(int j = i; j < n; j++)
+//     {
+//         if(arr[j] == 1)
+//         { 
+//             curr++;
+//         }
+//         else
+//         {
+//             break;
+//         }
+//     }
+//     res = max(res, curr);
+//    }
+   
+//    return res;
+// }
+
+// int main()
+// {
+//     int arr[] = {0,1,1,0,1,0};
+//     cout<<getMax(arr,6);
+// }
+
+//Efficient Sol.
 #include<iostream>
 using namespace std;
-int getMax(int arr[], int n)
+int getMaxConsecutive(int arr[], int n)
 {
-   int res = 0;
-   for(int i = 0; i < n; i++)
-   {
-    int curr = 0;
-    for(int j = i; j < n; j++)
+    int res = 0;
+    for(int i = 0; i < n; i++)
     {
-        if(arr[j] == 1)
+        int curr;
+        if(arr[i] == 0)
         {
-            curr++;
+            curr = 0;
         }
-        else
-        {
-            break;
+        else{
+            curr++;
+            res = max(res,curr);
         }
     }
-    res = max(res, curr);
-   }
-   
-   return res;
+    return res;
 }
 
 
@@ -28,6 +56,6 @@ int getMax(int arr[], int n)
 
 int main()
 {
-    int arr[] = {0,1,1,0,1,0};
-    cout<<getMax(arr,6);
+    int arr[] = {0,1,1,0,1,1,1};
+    cout<<getMaxConsecutive(arr,7);
 }
