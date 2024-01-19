b44b828d4e99cc4ebb860f581fd931308810ed8e
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-01-19 23:46:47 +0530

    Print max sum of subarray

--- maxSubArray.cpp
+++ maxSubArray.cpp
@@ -1,28 +1,53 @@
+// #include<iostream>
+// using namespace std;
+
+// int maxSub(int arr[], int n)
+// {
+//     int res = arr[0];
+//     for(int i = 0; i < n; i++)
+//     {
+//         int curr = 0;
+//         for(int j = i; j < n; j++)
+//         {
+//             curr = curr + arr[j];
+//             res = max(res,curr);
+
+//         }
+
+//     }
+//     return res;
+// }
+
+
+
+
+// int main()
+// {
+//     int arr[] = {2,3,-8,7,-1,2,3};
+//     cout<<maxSub(arr,7);
+// }
+
+
+//Efficient Approach
+
 #include<iostream>
 using namespace std;
 
-int maxSub(int arr[], int n)
+int maxSum(int arr[], int n)
 {
     int res = arr[0];
-    for(int i = 0; i < n; i++)
+    int maxEnding = arr[0];
+    for(int i = 1; i < n; i++)
     {
-        int curr = 0;
-        for(int j = i; j < n; j++)
-        {
-            curr = curr + arr[j];
-            res = max(res,curr);
-
-        }
-
+        maxEnding = max(maxEnding,arr[i]);
+        res = max(res,maxEnding);
     }
     return res;
 }
 
 
-
-
 int main()
 {
-    int arr[] = {2,3,-8,7,-1,2,3};
-    cout<<maxSub(arr,7);
+    int arr[] = {-3,8,-2,4,-5,6};
+    cout<<maxSum(arr,6);
 }
