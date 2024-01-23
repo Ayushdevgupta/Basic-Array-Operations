222c7d07936efff4c66c3e4388416f66884b0cb9
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-01-23 12:58:41 +0530

    Print max length alternating even odd sub array

--- maxEvenOddSubArray.cpp
+++ maxEvenOddSubArray.cpp
@@ -30,34 +30,58 @@
 // }
 
 //Efficient sol
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int maxEvenOdd(int arr[], int n)
+// int maxEvenOdd(int arr[], int n)
+// {
+//     int res = 1;
+//     int curr = 1;
+//     for(int i = 1; i < n; i++)
+//     {
+//         if((arr[i] % 2 == 0 && arr[i-1] != 0)||
+//         (arr[i] % 2 != 0 && arr[i-1] == 0))
+//         {
+//             curr++;
+//             res = max(res,curr);
+//         }
+//         else
+//         curr = 1;
+//     }
+//     return res;
+// }
+
+
+
+
+
+// int main()
+// {
+//     int arr[] = {5,10,20,6,3,8};
+//     cout<<maxEvenOdd(arr,6);
+
+// }
+
+//My Approach
+#include <iostream>
+using namespace std;
+int getMaxOddEven(int arr[], int n)
 {
     int res = 1;
     int curr = 1;
-    for(int i = 1; i < n; i++)
+    for(int i = 0; i < n-1; i++)
     {
-        if((arr[i] % 2 == 0 && arr[i-1] != 0)||
-        (arr[i] % 2 != 0 && arr[i-1] == 0))
+        if((arr[i] % 2 == 0 && arr[i+1] % 2 != 0 ||
+        arr[i] % 2 != 0 && arr[i+1] % 2 == 0))
         {
             curr++;
-            res = max(res,curr);
         }
-        else
-        curr = 1;
+        res = max(res,curr);
     }
     return res;
 }
 
-
-
-
-
-int main()
-{
-    int arr[] = {5,10,20,6,3,8};
-    cout<<maxEvenOdd(arr,6);
-
+int main() {
+  int arr[] = {13,13,12,15,18};
+  cout<<getMaxOddEven(arr,5);
 }
