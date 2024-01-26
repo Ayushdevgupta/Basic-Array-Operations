5596c3d62200ee147d78e46ee808d828d246b2ff
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-01-26 16:20:14 +0530

    Print max sum of circular subarray

--- maxCircularSubarraySum.cpp
+++ maxCircularSubarraySum.cpp
@@ -42,7 +42,7 @@ int normalMaxSum(int arr[], int n)
     int maxEnding = arr[0];
     for(int i = 1; i < n; i++)
     {
-        maxEnding = max(maxEnding,arr[i]);
+        maxEnding = max(maxEnding+arr[i],arr[i]);
         res = max(res,maxEnding);
     }
     return res;
