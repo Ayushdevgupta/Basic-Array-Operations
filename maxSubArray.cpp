d70d22f2450c5fe624944213ac38e235ff73945c
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-01-26 16:17:11 +0530

    Print max sub array sum

--- maxSubArray.cpp
+++ maxSubArray.cpp
@@ -39,7 +39,7 @@ int maxSum(int arr[], int n)
     int maxEnding = arr[0];
     for(int i = 1; i < n; i++)
     {
-        maxEnding = max(maxEnding,arr[i]);
+        maxEnding = max(maxEnding+arr[i],arr[i]);
         res = max(res,maxEnding);
     }
     return res;
