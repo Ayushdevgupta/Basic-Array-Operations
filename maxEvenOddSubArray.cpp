809c864d989212fffc6b5366ba20ef7956151e35
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-01-28 22:22:43 +0530

    Print max sum of Even odd sub array

--- maxEvenOddSubArray.cpp
+++ maxEvenOddSubArray.cpp
@@ -71,8 +71,8 @@ int getMaxOddEven(int arr[], int n)
     int curr = 1;
     for(int i = 0; i < n-1; i++)
     {
-        if((arr[i] % 2 == 0 && arr[i+1] % 2 != 0 ||
-        arr[i] % 2 != 0 && arr[i+1] % 2 == 0))
+        if((arr[i] % 2 == 0 && arr[i+1] % 2 != 0)||
+        (arr[i] % 2 != 0 && arr[i+1] % 2 == 0))
         {
             curr++;
         }
@@ -82,6 +82,6 @@ int getMaxOddEven(int arr[], int n)
 }
 
 int main() {
-  int arr[] = {13,13,12,15,18};
-  cout<<getMaxOddEven(arr,5);
+  int arr[] = {13,10,111};
+  cout<<getMaxOddEven(arr,3);
 }
