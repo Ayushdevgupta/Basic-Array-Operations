6ebab21931c4e80960406a5bd3e9abee3c2e946a
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-02-04 23:33:26 +0530

    Print max sum of k consequtive elem

--- slidingWindow.cpp
+++ slidingWindow.cpp
@@ -1,24 +1,51 @@
+//Naive Sol.
+// #include<iostream>
+// #include<climits>
+// using namespace std;
+// int maxSum(int arr[], int n, int k)
+// {
+//     int res = INT_MIN;
+//     for(int i = 0; i+k-1 < n; i++)
+//     {
+//         int curr = 0;
+//         for(int j = 0; j < k; j++)
+//         {
+//             curr += arr[i+j];
+//         }
+//         res = max(res,curr);
+//     }
+//     return res;
+// }
+
+
+
+
+
+// int main()
+// {
+//     int arr[] = {10,5,-2,20,1};
+//     int k = 3;
+//     cout<<maxSum(arr,5,k);
+// }
+
+//Efficient sol.
 #include<iostream>
-#include<climits>
 using namespace std;
 int maxSum(int arr[], int n, int k)
 {
-    int res = INT_MIN;
-    for(int i = 0; i+k-1 < n; i++)
-    {
-        int curr = 0;
-        for(int j = 0; j < k; j++)
-        {
-            curr += arr[i+j];
-        }
-        res = max(res,curr);
-    }
-    return res;
+int curr = 0;
+for(int i = 0; i < k; i++)
+{
+    curr+=arr[i];
 }
-
-
-
-
+int res = curr;
+for(int i = k; i < n; i++)
+{
+    curr = curr + arr[i] - arr[i-k];
+    res = max(res,curr);
+}
+return res;
+}
 
 int main()
 {
