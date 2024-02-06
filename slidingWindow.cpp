fd19b68fa4b9f8d4d4de6fb6b18a5eba287a3fae
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-02-07 00:22:12 +0530

    Find max sum of k consecutive elem

--- slidingWindow.cpp
+++ slidingWindow.cpp
@@ -33,19 +33,21 @@
 using namespace std;
 int maxSum(int arr[], int n, int k)
 {
-int curr = 0;
-for(int i = 0; i < k; i++)
-{
-    curr+=arr[i];
+    int curr = 0;
+    for(int i = 0; i < k; i++)
+    {
+        curr += arr[i];
+    }
+    int res = curr;
+    for(int i = k; i < n; i++)
+    {
+        curr = curr + arr[i] - arr[i-k];
+        res = max(res,curr);
+    }
+    return res;
+
 }
-int res = curr;
-for(int i = k; i < n; i++)
-{
-    curr = curr + arr[i] - arr[i-k];
-    res = max(res,curr);
-}
-return res;
-}
+
 
 int main()
 {
--- subarrayWithSum.cpp
+++ subarrayWithSum.cpp
@@ -1,35 +1,3 @@
-//My approach
-#include<iostream>
-using namespace std;
-void subarraySum(int arr[], int n, int sum)
-{
-    for(int i = 0; i < n; i++)
-    {
-        int res = arr[i];
-        for(int j = i + 1; j < n; j++)
-        {
-            res = res + arr[j];
-            if(res == sum)
-            {
-                cout<<"Yes";
-                return;
-            }
-        }
-    }
-    cout<<"No";
-}
-
-
-
-
-int main()
-{
-    int arr[] = {2,4};
-    int sum = 6;
-    subarraySum(arr,2,sum);
-}
-
-
 //Naive Approach
 // #include<iostream>
 // using namespace std;
@@ -37,28 +5,56 @@ int main()
 // {
 //     for(int i = 0; i < n; i++)
 //     {
-//         int curr = 0;
-//         for(int j = i; j < n-1; j++)
+//         int res = 0;
+//         for(int j = i; j < n; j++)
 //         {
-//             curr = curr + arr[j];
-//         }
-//         if(curr == sum)
-//         {
-//             cout<<"Yes";
-//             return;
+//             res = res + arr[j];
+//             if(res == sum)
+//             {
+//                 cout<<"Yes";
+//                 return;
+//             }
 //         }
 //     }
 //     cout<<"No";
 // }
 
-
-
-
-
-
 // int main()
 // {
-//     int arr[] = {2,4};
-//     int sum = 6;
-//     subarraySum(arr,2,sum);
+//     int arr[] = {1,2,3};
+//     int sum = 5;
+//     subarraySum(arr,3,sum);
 // }
+
+//Efficient Approach
+#include<iostream>
+using namespace std;
+int subarraySum(int arr[], int n, int sum)
+{
+    int s = 0;
+    int curr = 0;
+    for(int i = 0; i < n; i++)
+    {
+        curr += arr[i];
+        while(sum<curr)
+        {
+            curr -= arr[s];
+        }
+        if(curr == sum)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+int main()
+{
+    int arr[] = {1,2,3};
+    int sum = 5;
+    cout<<subarraySum(arr,3,sum);
+}
+
+
