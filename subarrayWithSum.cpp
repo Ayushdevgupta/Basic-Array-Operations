9b3f3daa5f9ab7d6f8f2bd731f6fd12ce784ea38
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-02-10 13:01:56 +0530

    Check subarray sum is equal to given sum or not

--- subarrayWithSum.cpp
+++ subarrayWithSum.cpp
@@ -33,12 +33,13 @@ int subarraySum(int arr[], int n, int sum)
 {
     int s = 0;
     int curr = 0;
-    for(int i = 0; i < n; i++)
+    for(int e = 0; e < n; e++)
     {
-        curr += arr[i];
+        curr += arr[e];
         while(sum<curr)
         {
             curr -= arr[s];
+            s++;
         }
         if(curr == sum)
         {
@@ -52,9 +53,9 @@ int subarraySum(int arr[], int n, int sum)
 
 int main()
 {
-    int arr[] = {1,2,3};
-    int sum = 5;
-    cout<<subarraySum(arr,3,sum);
+    int arr[] = {4,8,12,5};
+    int sum = 17;
+    cout<<subarraySum(arr,4,sum);
 }
 
 
