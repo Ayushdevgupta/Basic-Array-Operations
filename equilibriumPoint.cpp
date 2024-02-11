2a638bd68e79a22d9948217db842e1afc41d678e
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-02-11 23:50:52 +0530

    find out array has equilibrium point or not

---
+++ equilibriumPoint.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+bool ePoint(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        int ls = 0, rs = 0;
+        for(int j = 0; j < i; j++)
+        {
+            ls += arr[j];
+        }
+        for(int k = i+1; k < n; k++)
+        {
+            rs += arr[k];
+        }
+        if(ls == rs)
+        return true;
+    }
+    return false;
+}
+int main()
+{
+    int arr[] = {3,4,8,-9,9,7};
+    cout<<ePoint(arr,6);
+}
