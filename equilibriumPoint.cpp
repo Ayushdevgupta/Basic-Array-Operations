578c3b3d104ad6faf0d38c786885ff8e78372dac
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-02-12 23:04:03 +0530

    find out array has equilibrium point or not

--- equilibriumPoint.cpp
+++ equilibriumPoint.cpp
@@ -1,23 +1,55 @@
+//Naive Approach
+// #include<iostream>
+// using namespace std;
+// bool ePoint(int arr[], int n)
+// {
+//     for(int i = 0; i < n; i++)
+//     {
+//         int ls = 0, rs = 0;
+//         for(int j = 0; j < i; j++)
+//         {
+//             ls += arr[j];
+//         }
+//         for(int k = i+1; k < n; k++)
+//         {
+//             rs += arr[k];
+//         }
+//         if(ls == rs)
+//         return true;
+//     }
+//     return false;
+// }
+// int main()
+// {
+//     int arr[] = {3,4,8,-9,9,7};
+//     cout<<ePoint(arr,6);
+// }
+
+
 #include<iostream>
 using namespace std;
+
 bool ePoint(int arr[], int n)
 {
+    int rs = 0;
     for(int i = 0; i < n; i++)
     {
-        int ls = 0, rs = 0;
-        for(int j = 0; j < i; j++)
-        {
-            ls += arr[j];
-        }
-        for(int k = i+1; k < n; k++)
-        {
-            rs += arr[k];
-        }
+        rs += arr[i];
+    }
+    int ls = 0;
+    for(int i = 0; i < n; i++)
+    {
+        rs -= arr[i];
         if(ls == rs)
-        return true;
+        {
+            return true;
+        }
+        ls += arr[i];
     }
     return false;
 }
+
+
 int main()
 {
     int arr[] = {3,4,8,-9,9,7};
