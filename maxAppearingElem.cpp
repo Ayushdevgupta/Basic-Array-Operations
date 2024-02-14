1280d543733dff9e2c1c3b7c9e1db47e7ee41d11
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-02-14 23:30:30 +0530

    max appearing elem in ranges

--- maxAppearingElem.cpp
+++ maxAppearingElem.cpp
@@ -1,25 +1,57 @@
+//Naive Sol.
+// #include<iostream>
+// using namespace std;
+// int maxAppear(int left[], int right[], int n)
+// {
+//     int freq[100] = {0};
+//     for(int i = 0; i < n; i++)
+//     {
+//         for(int j = left[i]; j <= right[i]; ++)
+//         {
+//             freq[j] += 1;
+//         }
+//     }
+//     int res = 0;
+//     for(int i = 1; i < 100; i++)
+//     {
+//         if(freq[i] > freq[res])
+//         {
+//             res = i;
+//         }
+//     }
+//     return res;
+// }
+
+// int main()
+// {
+//     int left[] = {1,2,5,15};
+//     int right[] = {5,8,7,18};
+//     cout<<maxAppear(left,right,4);
+// }
+
+//Efficient Sol.
 #include<iostream>
 using namespace std;
-int maxAppear(int left[], int right[], int n)
+int maxAppear(int left[], int right[],int n)
 {
-    int freq[100] = {0};
+    int freq[101] = {0};
     for(int i = 0; i < n; i++)
     {
-        for(int j = left[i]; j <= right[i]; j++)
-        {
-            freq[j] += 1;
-        }
+        freq[left[i]]++;
+        freq[right[i]+1]--;
     }
     int res = 0;
     for(int i = 1; i < 100; i++)
     {
+        freq[i] = freq[i-1] + freq[i];
         if(freq[i] > freq[res])
-        {
-            res = i;
-        }
+        res = i;
     }
     return res;
 }
+
+
+
 
 int main()
 {
